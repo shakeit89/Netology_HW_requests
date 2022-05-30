3a2dcdb9e483567e5f6a1fd33767d99181951321
Ruslan Shakirov <shakirov3000@mail.ru>
2022-05-31 02:05:11 +0500

    init hw1

---
+++ HW1_request_heroes.py
@@ -0,0 +1,31 @@
+from superhero_api import Superhero
+
+
+hulk = Superhero('Hulk')
+captain_america = Superhero('Captain America')
+thanos = Superhero('Thanos')
+spider_man = Superhero('Spider-Man')
+error = Superhero('error')
+
+hulk.get_intelligence()
+captain_america.get_intelligence()
+thanos.get_intelligence()
+spider_man.get_intelligence()
+error.get_intelligence()
+
+def the_most_intel(*args):
+    list_of_heroes = [*args]
+    #удаляем несуществующих супергероев из списка
+    for i in reversed(list_of_heroes):
+        if i.intelligence == 0:
+            list_of_heroes.remove(i)
+    list_of_heroes.sort(key=lambda x: x.intelligence, reverse=True)
+    if not list_of_heroes:
+        print('К сожалению, ни у одного из супергероев невозможно узнать параметры')
+        return
+    print(f"Среди {' ,'.join([x.name for x in list_of_heroes])} "
+          f"самый умный: {list_of_heroes[0].name} с умом {list_of_heroes[0].intelligence}")
+    return list_of_heroes[0]
+
+if __name__ == "__main__":
+    the_most_intel(hulk, captain_america, spider_man, thanos, error)
---
+++ superhero_api.py
@@ -0,0 +1,53 @@
+import requests
+from tokens import token_superhero
+from pprint import pprint
+
+TOKEN = token_superhero
+
+
+class Superhero:
+
+    def __init__(self, name, token=TOKEN):
+        self.token = token
+        self.name = name
+        self.id = None
+        self.intelligence = None
+
+    def get_hero_id(self):
+        url = f'https://superheroapi.com/api/{self.token}/search/{self.name}'
+        try:
+            response = requests.get(url, timeout=5)
+        except requests.ConnectionError:
+            print(f'Нет соединения с сервером. Супергерой {self.name} не будет учтен в дальнейшем')
+            self.id = ''
+            return self.id
+        response.raise_for_status()
+        if response.status_code < 200 or response.status_code >= 300:
+            print(f'При получении данных возникла ошибка {response.status_code}, супергерой {self.name} не будет '
+                  f'учтен в дальнейшем')
+            self.id = ''
+            return self.id
+        if response.json()['response'] != 'error':
+            self.id = response.json()['results'][0]['id']
+            return self.id
+        print(f'Супергероя по имени {self.name} не существует')
+        self.id = ''
+
+    def get_intelligence(self):
+        self.get_hero_id()
+        if not self.id:  # проверка на несуществующего супергероя
+            self.intelligence = 0
+            return 0
+        url = f'https://superheroapi.com/api/{self.token}/{self.id}/powerstats'
+        response = requests.get(url)
+        response.raise_for_status()
+        r = response.json()
+        self.intelligence = int(r['intelligence'])
+        return self.intelligence
+
+
+if __name__ == "__main__":
+    hulk = Superhero('Hulk')
+    pprint(hulk.get_hero_id())
+    pprint(hulk.get_intelligence())
+    print(hulk.id, hulk.intelligence)
