01789b463070adc3f78a5d703f5fa0a798f59d15
Ruslan Shakirov <shakirov3000@mail.ru>
2022-06-02 03:00:22 +0500

    added check of internet connection

--- HW2_yandex_disc.py
+++ HW2_yandex_disc.py
@@ -1,7 +1,23 @@
 from yandex_disc import YaUploader
 from tokens import token_yandex
+import os
+import time
+from tqdm import tqdm
 
+DIR_NAME = 'files'
+BASE_PATH = os.getcwd()
+file_names = os.listdir(DIR_NAME)
 TOKEN = token_yandex  # указываем свой токен
 uploader = YaUploader(TOKEN)
-path_to_file = 'files/test_ya_disc.txt'
-uploader.upload(path_to_file)
+
+
+t = tqdm(total=len(file_names))
+
+for files in file_names:
+    path_to_file = os.path.join(BASE_PATH, DIR_NAME, files)
+    uploader.upload(path_to_file)
+    t.update(1)
+t.close()
+
+print(f'Все файлы из папки {os.path.join(BASE_PATH, DIR_NAME, files)} загружены')
+
--- yandex_disc.py
+++ yandex_disc.py
@@ -1,6 +1,7 @@
 import sys
 from tokens import token_yandex
 import requests
+import datetime
 
 TOKEN = token_yandex
 
@@ -27,7 +28,9 @@ class YaUploader:
             sys.exit()
 
         if r.status_code == 200:
-            print('Связь с сервером установлена')
+            with open('log.txt', 'a') as log:
+                log.write(
+                    f'{datetime.datetime.now().strftime("%H:%M:%S:%f %d/%m/%Y")} | Связь с сервером установлена\n')
         else:
             print(f'Внимание! ошибка {r.status_code}')
             print(r.json()['message'])
@@ -35,15 +38,18 @@ class YaUploader:
 
         return r.json()['href']
 
-    def get_file_name_from_path(self, path):  # возвращает имя файла из пути (для Windows!)
-        return path[path.rfind('/') + 1:]
+    def _get_file_name_from_path(self, path):  # возвращает имя файла из пути (для Windows!)
+        return path[path.rfind('\\') + 1:]
 
     def upload(self, upload_file: str):
-        file_name = self.get_file_name_from_path(upload_file)
+        file_name = f"/files_for_netology/" + self._get_file_name_from_path(upload_file)
         with open(upload_file, 'rb') as f:
             r = requests.put(self.get_link_to_upload(path=file_name), data=f.read())
             if r.status_code == 201:
-                print(f'Файл {file_name} успешно загружен на Я.Диск')
+                with open('log.txt', 'a') as log:
+                    log.write(
+                        f'{datetime.datetime.now().strftime(f"%H:%M:%S:%f %d/%m/%Y")} | Файл '
+                        f'{file_name[file_name.rindex("/") + 1:]} успешно загружен на Я.Диск\n')
             else:
                 print(f'При загрузке файла произошла ошибка {r.status_code}')
 
